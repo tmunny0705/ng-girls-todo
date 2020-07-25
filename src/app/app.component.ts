416e4c73c64c051136406b0d7a23410ebeaad0e5
Tanja Petrovic <tpetrovic0705@gmail.com>
2020-07-25 16:25:32 -0500

    Finished step 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,26 @@ import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
     </h1>
 
     <app-input-button-unit></app-input-button-unit>
+
+    <ul>
+      <li *ngFor="let todoItem of todoList">
+        <app-todo-item [item]="todoItem"></app-todo-item>
+      </li>
+    </ul>
   `,
 
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo-list';
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+    {title: 'pizza party'},
+  ];
 }
 
