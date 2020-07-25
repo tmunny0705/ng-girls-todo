d28b28c5e28d1b0ae36a7a93ccac1eeb6f9a2b0f
Tanja Petrovic <tpetrovic0705@gmail.com>
2020-07-25 12:01:20 -0500

    Your Message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { PrefixNot } from '@angular/compiler';
+import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>
+      Welcome to {{title}}!
+    </h1>
+
+    <app-input-button-unit></app-input-button-unit>
+  `,
+
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo-list';
 }
+
